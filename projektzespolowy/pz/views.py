6d7154c31ff044b76e31f6111f7af360c935b41b
Gruntag <kuba.moskal@gmail.com>
2021-11-26 00:08:33 +0100

    Dynamiczna generacja produktów

--- projektzespolowy/pz/views.py
+++ projektzespolowy/pz/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
+from .models import *
+
 
 def store(request):
-    context = {}
+    products = Product.objects.all
+    context = {'products': products}
     return render(request, 'pz/store.html', context)
+
 
 def cart(request):
     context = {}
     return render(request, 'pz/cart.html', context)
+
 
 def checkout(request):
     context = {}
